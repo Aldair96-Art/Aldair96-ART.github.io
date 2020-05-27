ab0a664bcbab3a1e17595fc3e2ea5a55b89e61dc
Aldair96-Art <aldair96@byui.edu>
2020-05-27 10:26:08 -0600

    update

---
+++ lesson6/Js/Nav.js
@@ -0,0 +1,21 @@
+function toggle_nav() {
+    document.getElementById("main_nav").classList.toggle("nav_hidden");
+ }
+
+ const date = new Date();
+ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+ const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+ const dayName = days[date.getDay()];
+ const monthName = monthNames[date.getMonth()];
+ const year = date.getFullYear();
+ document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
+ document.getElementById("year").innerHTML = year;
+
+ var d = new Date();
+const banner = document.getElementById("banner");
+if (d.getDay() == 5) {
+    pancakebanner.setAttribute("style", "display: block");
+}
+else {
+    pancakebanner.setAttribute("style", "display: none"); 
+}
