ff09a2d5c2b69100a1c23890d8c176e1ecff2e5c
Aldair96-Art <aldair96@byui.edu>
2020-05-28 10:04:56 -0600

    updated

--- lesson6/Js/windchill.js
+++ lesson6/Js/windchill.js
@@ -1,34 +1,13 @@
-/*let windchill = 0;
-
-(function () 
-    const hightemp = document.getElementById("hightemp").innerHTML;
-    const windspeed = document.getElementById("windspeed").innerHTML;
-    windchill = 35.74 + (0.6215 * hightemp) - (35.75 * (windspeed ** .16)) + (0.4275 * hightemp * (windspeed ** .16));
+(function () {
+    const High = document.getElementById('High').innerHTML;
+    const wind = document.getElementById('wind').innerHTML;
+    let windChill = 35.74 + (0.6215 * High) - (35.75 * (wind ** .16)) + (0.4275 * High * (wind ** .16));
     
      
-    if (hightemp <= 50 && windspeed > 3) {
-       windchill = Math.round(windchill);
+    if (High <= 50 && wind > 3) {
+       windChill = Math.round(windChill);
     } else {
-       windchill = "NA";
+       windChill = "NA";
     }
-    document.getElementById("windchill").innerHTML = windchill;*/
-  //}());
-
-  let hightemp = 41;
-let windspeed = 8;
-let windchill = 0;
-
-if (hightemp <= 50 && windspeed > 3){
-windchill = 35.74 + .6215 * hightemp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * hightemp
- * Math.pow(windspeed,.16);
-}
-
-document.getElementById("hightemp").innerHTML = hightemp + '&deg';
-document.getElementById("windspeed").innerHTML = windspeed + " mph"
-
-if(windchill === 0){
-document.getElementById("windchill").innerHTML = 'N/A';
-}
-else{
-document.getElementById("windchill").innerHTML = Math.round(windchill) + '&deg;';
-}
+    document.getElementById('windChill').innerHTML = windChill;
+  }());
