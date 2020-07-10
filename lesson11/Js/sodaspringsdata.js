1b5701d9bdd23f4e374a9f935b2f0b82dab39d8a
Aldair96-Art <aldair96@byui.edu>
2020-07-10 01:52:46 -0600

    fixed-week11

--- lesson11/Js/sodaspringsdata.js
+++ lesson11/Js/sodaspringsdata.js
@@ -1,49 +1,51 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=4a32e867e085eb3d905c18e7e6de00d4";
 fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        document.getElementById('Currently').textContent = Math.floor(jsObject.main.temp);
-        document.getElementById('Temp').textContent = Math.floor(jsObject.main.temp_max);
-        document.getElementById('Humidity').textContent = jsObject.main.humidity;
-        document.getElementById('WindSpeed').textContent = Math.ceil(jsObject.wind.speed);
+.then((response) => response.json())
+.then((weather) => {
+    document.getElementById('current-temp').textContent = Math.round(weather.main.temp);
+    document.getElementById('High').textContent = Math.round(weather.main.temp_max);
+    document.getElementById('Humidity').textContent = weather.main.humidity;
+    document.getElementById('WindSpeed').textContent = Math.ceil(weather.wind.speed);
 
-        var temp = parseFloat(document.getElementById("Temp").innerHTML);
-        var speed = parseFloat(document.getElementById("WindSpeed").innerHTML);
-        var chill = 35.74 + .6215 * temp - 35.75 * Math.pow(speed, .16) +  .4275 * temp * Math.pow(speed, .16);
-        if (temp < 51 && speed > 3) {
-            document.getElementById("WindChill").innerHTML = Math.round(chill);
-        }
-        else {
-            document.getElementById("WindChill").innerHTML = "N/A";
-        }
-    });
+    //code to calculate Windchill
 
-const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=b2df84ca600590f5e0eb74b31fdf2289";
+   const High = document.getElementById('High').innerHTML;
+   const WindSpeed = document.getElementById('WindSpeed').innerHTML;
+   var windchill = 35.74 + (0.6215 * High) - (35.75 *  Math.pow(WindSpeed ** .16)) + (0.4275 * High *  Math.pow(WindSpeed ** .16));
+   
+    if (High <= 50 && WindSpeed > 3) {
+      windchill = Math.round(windchill) + '°F';
+   } else {
+      windchill = "N/A";
+   }
+
+    document.getElementById('WindChill').innerHTML = windchill;
+
+});
+
+//Forecast//
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=4a32e867e085eb3d905c18e7e6de00d4";
 fetch(forecastURL)
     .then((response) => response.json())
-    .then((jsonObject) => {
+    .then((jsObject) => {
 
-        const fiveDay = jsonObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        console.log(fiveDay);
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
 
         const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        let day = 1;
 
-        fiveDay.forEach(forecast => {
-            let date = forecast.dt_txt;
-            let d = new Date(date).getDay();
-            let f = forecast.main.temp;
-            const icon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        let day = 0;
+        fivedayforecast.forEach(forecast => {
+            var d = new Date(forecast.dt_txt);
+            var currentday = new Date(d).getDay();
+            const img = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
 
-            console.log(icon);
-
-            document.getElementById(`fore${day}`).textContent = weekdays[d];
-            document.getElementById(`forecast${day}`).textContent = Math.floor(f);
-            document.getElementById(`imagesrc${day}`).setAttribute('alt', forecast.weather[0].description);
-            document.getElementById(`imagesrc${day}`).setAttribute('src', icon);
+            document.getElementById(`fore${day+1}`).textContent = weekdays[currentday];
+            document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast.main.temp);
+            document.getElementById(`imagesrc${day+1}`).setAttribute('alt', forecast.weather[0].description);
+            document.getElementById(`imagesrc${day+1}`).setAttribute('src', img);
             day++;
             d++;
-        })
+        });
     });
 
 
